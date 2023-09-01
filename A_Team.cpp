9479f51388000d512b707e2021635daadace6710
Niraj Kumar <nirajs9kr@gmail.com>
2023-09-02 00:04:16 +0530

    done

--- A_Team.cpp
+++ A_Team.cpp
@@ -1,1 +1,25 @@
-#include
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string str;
+        cin >> str;
+
+        int one = count(str.begin(), str.end(), '1');
+        int zero = count(str.begin(), str.end(), '0');
+        cout << one << " " << zero << endl;
+        for (int i = 0; i < t; i++)
+        {
+            
+        }
+        
+    }
+    return 0;
+}
