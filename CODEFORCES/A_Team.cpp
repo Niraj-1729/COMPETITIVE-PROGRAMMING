2f5336349cef49c3171d31768b7ae893feebc663
Niraj Kumar <nirajs9kr@gmail.com>
2023-09-02 21:51:13 +0530

    .

--- CODEFORCES/A_Team.cpp
+++ CODEFORCES/A_Team.cpp
@@ -1,27 +1,21 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     int t;
     cin >> t;
+    int count=0;
     while (t--)
     {
-    int c = 0;
-        string str;
-        cin >> str;
-
-        int one = count(str.begin(), str.end(), '1');
-        int zero = count(str.begin(), str.end(), '0');
-        // cout << one << " " << zero << endl;
-        if (one >= 2)
+        int a,b,c;
+        cin>>a>>b>>c;
+        if (a+b==2|| b+c==2|| c+a==2)
         {
-            c++;
+            count+=1;
         }
-         
-    cout<<c;
+        
     }
+    cout<<count;
     return 0;
 }
